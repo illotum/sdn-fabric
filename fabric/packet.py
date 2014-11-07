7d7d1756831f1a9caf0631fcac5cf0c9049ccf5b
tarungumma <gumma@ualberta.ca>
2014-11-06 19:35:34 -0700

    Update packet.py

--- fabric/packet.py
+++ fabric/packet.py
@@ -7,17 +7,40 @@ from ryu.lib.packet import ethernet
 from ryu.lib.packet import arp
 from ryu.lib.packet import lldp
 
-def new_lldp():
+def create_lldp(dpid,port_no=DEFAULT_VALUE):
+    '''
+    Ryu.packet_lldp()
+    '''
     pass
 
-def new_arp():
+def create_arp( dl_src,dl_dst,nl_src,nl_dst):
+    '''
+    => ARP()
+    '''
     pass
 
-def parse_eth():
+def parse_eth(data):
     pass
 
-def parse_lldp():
+def parse_lldp(decr,data):
+    '''
+    => (descr dictionary is built and descr is returned)
+    
+    '''
     pass
 
-def parse_arp():
+def parse_arp(descr,data):
+    '''
+    Same as parse_lldp
+    '''
+    pass
+
+def parse(data):
+    '''
+    {"dl_type:","dl_src":}
+    eth.header=()
+    if parse.arp()
+    elif parse_lldp()
+    call parse_arp()
+    '''
     pass
