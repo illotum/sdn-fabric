6c5320d05a4810cd1041074ece469dc0e7dd8b97
alex <alex.valiushko@cybera.ca>
2014-11-24 19:23:44 -0700

    refactor NetworkDB -> NetworkGraph; store only internal ports

--- fabric/network.py
+++ fabric/network.py
@@ -3,7 +3,6 @@ This module contains everything related to network topology processing
 """
 from collections import defaultdict
 import heapq
-from fabric.pqdict import PQDict
 
 
 class Network(object):
@@ -12,11 +11,10 @@ class Network(object):
     Container for all network state
     """
 
-    def __init__():
-        self.topo = TopologyDB()
-        self.ip_to_mac = defaultdict(None)  # IP => MAC
-        self.mac_to_port = defaultdict(None)  # MAC => (dpid,port_no)
-        list_of_paths_between_core_links = []
+    def __init__(self):
+        self.topo = TopologyGraph()  # (src_dpid, dst_dpid) => port_no
+        self.ip_to_mac = defaultdict()  # IP => MAC
+        self.mac_to_port = defaultdict()  # MAC => (dpid,port_no)
 
     def mac_of_ip(self, ip):
         """
@@ -43,7 +41,6 @@ class Network(object):
         return self.mac_to_port[mac]
 
     def parse_ports(ports):
-        self.topo[(dpid, port_no)] = (self._topo.P_LEARNING, None)
         pass
 
     def new_peer():
@@ -54,105 +51,74 @@ class Network(object):
         pass
 
 
-class TopologyDB(dict):
-
-    P_EDGE = 1
-    P_CORE = 2
-    P_LEARNING = 0
+class TopologyGraph(defaultdict):
     """
-    Topology graph with network related helpers
+    Topology graph with network related helpers.
+
+    Stores unidirectional (peerA, PeerB) => out_port mappings.
     """
-    def spf(lsdb):
-        '''
-        It stores a table of self_paths-> {(src,dst)=>[(dpid,port_no),(),()]}
 
-        returns a list of src,dst=> dpid,port_no
-        '''
-        neighbourTable = self.neighbour_discovery(lsdb)
-        for i in neighbourTable.keys():
-            for j in neighbourTable.keys():
-                if i is not j:
-                    self_paths[(i, j)] = self.shortestPath(
-                        neighbourTable, i, j)
-        return self_paths
+    def __init__(self, *args, **kwargs):
+        self.dpids = set()
+        super(TopologyGraph, self).__init__(*args, **kwargs)
 
-    def get_all_core(self, lsdb, coreLinks=[]):
+    @property
+    def edges(self):
         """
-        Returns all links in P_CORE state
+        Return a list of internal edges of the current network topology.
 
-        :returns: list of links
-        :rtype: list of (dpid, port_no)
+        :returns: unidirectional internal links that are discovered so far
+        :rtype: list of (peerA, peerB)
         """
-        for x in lsdb.keys():
-            if lsdb[x][0] is 2:
-                coreLinks.append((x, lsdb[x][1]))
-        return coreLinks
+        return self.keys()
 
-    def active_core_links(self, lsdb):
+    @property
+    def switches(self):
         """
-        Returns all links which are Active and connected to another switch
-        :returns: Dictionary of active_links
-        :return Type: Dictionary
+        Return a list of all switches in the topology.
+
+        :returns: a coll of dpids
+        :rtype: set of int
         """
-        activeLinks = {}
-        for x in lsdb.keys():
-            if lsdb[x][0] is 2:
-                activeLinks[x] = lsdb[x][1]
-        return activeLinks
+        return self.dpids
 
-    def neighbour_discovery(self, lsdb, topoTable={}):
+    def run_spf(self):
+        lst = [(a, b) for a in self.switches for b in self.switches if a != b]
+        self.paths = defaultdict
+        for src, dst in lst:
+            self.paths[src, dst] = self.dijkstra(src, dst)
+
+    def dijkstra(self, src, dst):
         """
-        :param lsdb: link state data base
-        :type lsdb: Dictionary
+        Compute the best path between two nodes.
 
-        :param topoTable: topology table.
-        :type topoTable: Dictionary
+        :param src: dpid of the starting switch
+        :type src: int
+
+        :param dst: dpid of the target switch
+        :type dst: int
+
+        :returns: best path to dst from src
+        :rtype: list of int
         """
-        dlink = self.active_core_links(lsdb)
-        for x, y in dlink.keys():
-            if x not in topoTable:
-                topoTable[x] = {dlink[(x, y)]: y}
-            else:
-                topoTable[x][dlink[(x, y)]] = y
-        return topoTable
+        graph = defaultdict(list)
+        for a, b in self.edges:
+            graph[a].append(b)
 
-    def shortestPath(self, G, start, end):
-        D, P = self.Dijkstra(G, start, end)
-        Path = []
-        while 1:
-            Path.append(end)
-            if end == start:
-                break
-            end = P[end]
-        Path.reverse()
-        Path = self.path_to_port(Path, G)
-        return Path
+        queue = [(0, src, [])]
+        seen = set()
 
-    def Dijkstra(self, G, start, end=None):
-        """
-        Computes Djikstras algorithm to get best paths
-        """
-        D = {}  # dictionary of final distances
-        P = {}  # dictionary of predecessors
-        Q = PQDict()   # est.dist. of non-final vert.
-        Q[start] = 0
-
-        for v in Q:
-            D[v] = Q[v]
-            if v == end:
-                break
-
-            for w in G[v]:
-                vwLength = D[v] + 1
-                if w in D:
-                    if vwLength < D[w]:
-                        raise ValueError, \
-                            "Dijkstra: found better path to already-final vertex"
-                elif (w not in Q) or (vwLength < Q[w]):
-                    Q[w] = vwLength
-                    P[w] = v
-
-        return D, P
+        while queue:
+            cost, a, path = heapq.heappop(queue)
+            if a not in seen:
+                seen.add(a)
+                path = path + [a]  # FIXME: Not the most CPU effective way
+                if a == dst:
+                    return path  # Success
+                for b in graph[a]:
+                    if b not in seen:
+                        heapq.heappush(queue, (cost+1, b, path))
+        return None  # Failure
 
     def path_to_port(self, path, G, count=0):
         newPath = []
